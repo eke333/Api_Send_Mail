4f34b8bed37bd55f85a0686e560a35a7725dd1bf
22505 <visionstrategie@gmail.com>
2024-07-13 00:08:06 +0000

    attachment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import base64
 from flask import Flask, request
 from flask_cors import CORS
 from flask_mail import Mail, Message
@@ -15,7 +16,7 @@ app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'visionstrategie@gmail.com'#'noreply@visionstrategie.com'
 app.config['MAIL_PASSWORD'] = 'dunp qbqo lnzb meki'#'ajvg phyd ufct haqc'#'Uz2M8rHMQ5Xyfyt'
-app.config['MAIL_DEFAULT_SENDER'] = 'visionstrategie@gmail.com'#'noreply@visionstrategie.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'noreply@visionstrategie.com'#'noreply@visionstrategie.com' ; 'visionstrategie@gmail.com'
 
 mail = Mail(app)
 
@@ -27,21 +28,25 @@ class HelloWorld(Resource):
 class SendMailResource(Resource):
     def post(self):
         try:
-            # Get data from the request JSON
             data = request.get_json()
             subject = data["subject"]
             recipient = data["recipient"]
             html_content = data["message"]
+            attachment = data.get("attachment")
+            filename = data.get("filename")
 
-            # Create a message object
             msg = Message(subject=subject, recipients=[recipient], html=html_content, sender='visionstrategie@gmail.com')
 
-            # Send the email
+            if attachment and filename:
+                decoded_attachment = base64.b64decode(attachment)
+                msg.attach(filename, "application/octet-stream", decoded_attachment)
+
             mail.send(msg)
 
             return {'message': 'Email sent successfully'}, 200
         except Exception as e:
             return {'error': str(e)}, 500
+
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(SendMailResource, '/send-mail')
